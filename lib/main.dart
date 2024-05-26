a23e7075a6564081d11ac58d05cae6263992d14f
Abe99 <i.mqami@gmail.com>
2024-05-26 02:16:46 +0100

    Basic UI

--- lib/main.dart
+++ lib/main.dart
@@ -8,13 +8,70 @@ class Quizzler extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Scaffold(
-            backgroundColor: Colors.grey[900],
-            body: SafeArea(
-              child: Padding(
-                padding: EdgeInsets.symmetric(horizontal: 10.0),
-                
-              ),
-            )));
+      home: Scaffold(
+        backgroundColor: Colors.grey[900],
+        body: SafeArea(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 10.0),
+            child: QuizPage(),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class QuizPage extends StatefulWidget {
+  const QuizPage({super.key});
+
+  @override
+  State<QuizPage> createState() => _QuizPageState();
+}
+
+class _QuizPageState extends State<QuizPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: <Widget>[
+        Expanded(
+          flex: 5,
+          child: Center(
+            child: const Text("This is where the question will go",
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontSize: 25.0,
+              color: Colors.white,
+            ),),
+          ),
+        ),
+        Expanded(
+          child: TextButton(
+            onPressed: () {
+              print("The user picked true");
+            },
+            child: Text('True',
+            style: TextStyle(
+              fontSize: 18.0,
+              color: Colors.green,
+            ),),
+          ),
+        ),
+         Expanded(
+          child: TextButton(
+            
+            onPressed: () {
+              print("The user picked false");
+            },
+            child: Text('False',
+            style: TextStyle(
+              fontSize: 18.0,
+              color: Colors.red,
+            ),),
+          ),
+        ),
+      ],
+    );
   }
 }
